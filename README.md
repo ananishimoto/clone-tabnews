ed89438434e6d3e04334a6aa189b0fa869dba73e
Ana Nishimoto <ananishimoto@users.noreply.github.com>
2024-07-16 09:26:24 +0000

    feat: add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Exercise in the curso.dev to create a clone of the Brazilan website www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>🌸Bom dia, flor do dia!🌸</h1>
+  return <h1>🌸Bom dia, flor do dia!🌸</h1>;
 }
 
 export default Home;
