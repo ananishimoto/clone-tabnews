d587e50545e4cbb4a7cc5f96946eb4105145d199
Ana Nishimoto <ananishimoto@users.noreply.github.com>
2024-07-17 12:57:16 +0000

    feat: added text for in construct page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>🌸Bom dia, flor do dia!🌸</h1>;
+  return (
+    <>
+      <h1>🌸Monamie, in construction🌸</h1>
+      <p>
+        This is a page where anyone can share their experiences, hobbies, and
+        other information
+      </p>
+      <p>
+        It's a place to connect people in a way to remind blogs from an
+        "old-style" internet, and to give some space from the high pace social
+        media we are used now
+      </p>
+    </>
+  );
 }
 
 export default Home;
